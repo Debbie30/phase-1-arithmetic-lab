e7b7d2fff80c3a7545ffa1484f1d2089d7597128
Debbie30 <cherotichdebby11@gmail.com>
2024-03-21 13:47:44 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+// Assign values to x and y
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
+let max = Math.max(5, 10, 20, 15);
